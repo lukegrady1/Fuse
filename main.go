d0806ef302fece9ab5ad3d0e41aee21ad4432839
lukegrady1 <lukegrady9@outlook.com>
2024-07-02 11:44:17 -0400

    commit

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -12,6 +16,27 @@ func main() {
 	log.Println("started")
 	repoRouter.HandleFunc("/hello", UserAuthentication).Methods("GET")
 	http.ListenAndServe(":8020", repoRouter)
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected")
+
+	// Get a handle for your database and collection
+	collection := client.Database("test").Collection("faq")
+
 }
 
 func UserAuthentication(w http.ResponseWriter, r *http.Request) {
